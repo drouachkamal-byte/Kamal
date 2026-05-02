5f2d2749531130170683bf719f1afc5c6569f103
drouachkamal-byte <drouachkamal@gmail.com>
2026-05-02 19:27:02 +0100

    Initialize Express server with webhook endpoints

---
+++ index.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+var PORT = process.env.PORT || 3000;
+app.use(express.json());
+app.get('/health', function(req, res){ res.json({status:'ok'}); });
+app.get('/webhook', function(req, res){ var mode = req.query['hub.mode']; var token = req.query['hub.verify_token']; var challenge = req.query['hub.challenge']; if(mode && token === process.env.WA_VERIFY_TOKEN){ res.status(200).send(challenge); }else{ res.sendStatus(403); } });
+app.post('/webhook', function(req, res){ res.sendStatus(200); });
+app.listen(PORT);
